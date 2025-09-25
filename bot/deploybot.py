daf3115c2414619607ecf0f075030c126067c798
Pavel <rainbow343@yandex.ru>
2025-09-25 17:59:59 +0300

    fix volume

--- bot/deploybot.py
+++ bot/deploybot.py
@@ -21,7 +21,7 @@ async def handle_deploy(message: telebot.types.Message):
 
     if user_id in ADMIN_IDS:
         try:
-            command = "echo /root/docker_restart.sh > docker_executor_host"
+            command = "echo /root/docker_restart.sh > executor_docker_host"
             os.system(command)
             response_text = "✅ *Успех*"
         except Exception:
